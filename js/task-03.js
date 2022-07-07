9447a29eedd0b157db824628b5de26f890668f53
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-07-07 18:25:27 +0300

    task 3 - finished

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,22 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Используй массив объектов images для создания элементов <img> 
+// вложенных в < li >.Для создания разметки используй шаблонные строки 
+// и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+const creatImg = ({ url, alt }) =>
+  `<li style = "margin-bottom:20px"><img src="${url}" alt="${alt}" style = "display:block; max-width:420px;"></li>`;
+
+const createImgArray = (array) => 
+  array.reduce((acc, item) =>
+    acc + creatImg(item), "")
+
+const galleryRef = document.querySelector('.gallery')
+galleryRef.setAttribute("style", "display:grid; display:flex; flex-direction:column; list-style:none; margin:0; padding:0");
+galleryRef.insertAdjacentHTML("afterbegin", createImgArray(images));
+
+
